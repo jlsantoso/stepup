2313c1b6d0239f267e1c3994b1756e5d84947b69
jlsantoso <joseluis.santos.cs@gmail.com>
2013-02-27 17:15:34 +0100

    Servlets

--- StepUpOpenBadges/src/org/be/kuleuven/hci/openbadges/utils/StepUpConstants.java
+++ StepUpOpenBadges/src/org/be/kuleuven/hci/openbadges/utils/StepUpConstants.java
@@ -4,4 +4,8 @@ public class StepUpConstants {
 
 	public static final String URLPUSHEVENT = "http://ariadne.cs.kuleuven.be/wespot-dev-ws/rest/pushEvent";
 	public static final String IMGPATH = "/img/badges/";
+	public static final String DIIGOVERB = "bookmarked";
+	public static final String BLOGPOST= "posted";
+	public static final String BLOGCOMMENT = "commented";
+	public static final String TWITTER = "tweeted";
 }
